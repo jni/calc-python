fa2987ce3f68606773037c8f60d112c1ba88d6e7
ashwanisng <44340485+ashwanisng@users.noreply.github.com>
2019-10-07 10:53:03 +0530

    Update calc.py
    op replaced by "operator". :)

--- calc.py
+++ calc.py
@@ -3,18 +3,18 @@ calc.py: A simple command-line calculator
 """
 import sys
 
-filename, num1, op, num2 = sys.argv
+filename, num1, operator, num2 = sys.argv
 num1, num2 = float(num1), float(num2)
 
-if op == '+':
+if operator == '+':
     print(num1 + num2)
-elif op == '-':
+elif operator == '-':
     print(num1 - num2)
-elif op == 'x':
+elif operator == 'x':
     print(num1 * num2)
-elif op == '/':
+elif operator == '/':
     print(num1 / num2)
-elif op == '^':
+elif operator == '^':
     print(num1 ** num2)
 else:
     print(0)
